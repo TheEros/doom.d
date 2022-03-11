eaaa6916d6cf8acc198d6ddc75435bc30c42df04
TheEros <5605058+zhou_linjun@user.noreply.gitee.com>
2022-03-11 14:50:18 +0800

    add elfeed search filter and format app.el

--- settings/app.el
+++ settings/app.el
@@ -1,5 +1,11 @@
 ;;; settings/app.el -*- lexical-binding: t; -*-
-;;用于配置rss elfeed
-(setq rmh-elfeed-org-files (list "~/.doom.d/org/elfeed/elfeed.org"))
+;; 用于配置rss elfeed
+(after! elfeed
+  (setq elfeed-search-filter "@2-years-ago"))
+
+;;用于配置rss elfeed-org
+(use-package! elfeed-org
+  :config
+  (setq rmh-elfeed-org-files (list "~/.doom.d/org/elfeed/elfeed.org")))
 
 (provide 'app)
